b39b75547bc447d3f1a1c77d9beaf7fc74b9d25d
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-09-11 22:39:35 +0530

    continue

--- threads_app/src/components/UserPost.jsx
+++ threads_app/src/components/UserPost.jsx
@@ -1,8 +1,17 @@
+import { Flex } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const UserPost = () => {
   return (
-    <div>UserPost</div>
+    <Link to={"/markzuckerberg/post/1"}>
+        <Flex gap={3} mb={4} py={5}>
+            <Flex flexDirection={"column"} ></Flex>
+
+        </Flex>
+    </Link>
   )
 }
 
 export default UserPost
+
+// 46:47
