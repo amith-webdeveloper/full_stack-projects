0747db8c39c73fc24a21d202ac085d9a1f287546
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-09-26 09:30:32 +0530

    setup for noteapp

--- threads_app/backend/controllers/userController.js
+++ threads_app/backend/controllers/userController.js
@@ -46,71 +46,75 @@ const signupUser = async (req, res) => {
   }
 };
 
-
 // loginUser
-const loginUser = async (req , res) => {
-    try {
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
 
-        const {username , password} = req.body;
-        const user = await User.findOne({username})
-        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid username or password" });
 
-        if(!user || !isPasswordCorrect) return res.status(400).json({message: "Invalid username or password"});
+    generateTokenAndSetCookie(user._id, res);
 
-        generateTokenAndSetCookie(user._id, res);
-
-        res.status(200).json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            username:user.username
-        })
-        
-    } catch (error) {
-        res.status(500).json({message:error.message})
-        console.log("Error in loginUser: ", error.message);
-        
-        
-    }
-
-}
-
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in loginUser: ", error.message);
+  }
+};
 
 // logoutUser
-const logoutUser =  (req , res) => {
-    try {
-        res.cookie("jwt" , "" , {maxAge:1});
-        res.status(200).json({message:"User logout successfully!"})
-    } catch (error) {
-        res.status(500).json({error : error.message})
-        console.log("Error in logoutUser: ", error.message);
-        
-        
-    }
-
-}
-
-
+const logoutUser = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logout successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in logoutUser: ", error.message);
+  }
+};
 
 // followUnFollowUser
 
-const followUnFollowUser = async (req , res) => {
-  
+const followUnFollowUser = async (req, res) => {
+  try{
+  const { id } = req.params;
+		const userToModify = await User.findById(id);
+		const currentUser = await User.findById(req.user._id);
 
-}
+		if (id === req.user._id.toString())
+			return res.status(400).json({ error: "You cannot follow/unfollow yourself" });
+
+		if (!userToModify || !currentUser) return res.status(400).json({ error: "User not found" });
+
+		const isFollowing = currentUser.following.includes(id);
+
+		if (isFollowing) {
+			// Unfollow user
+			await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+			await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+			res.status(200).json({ message: "User unfollowed successfully" });
+		} else {
+			// Follow user
+			await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+			await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+			res.status(200).json({ message: "User followed successfully" });
+		}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in followUnFollowUser: ", error.message);
+  }
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-export { signupUser , loginUser  , logoutUser , followUnFollowUser};
-
-
+export { signupUser, loginUser, logoutUser, followUnFollowUser };
--- threads_app/backend/routes/userRoutes.js
+++ threads_app/backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { followUnFollowUser, loginUser, logoutUser, signupUser } from "../controllers/userController.js"
+import protectRoute from "../middlewares/protectRoute.js"
 
 const router = express.Router()
 
 router.post("/signup" , signupUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
-router.post("/Follow", followUnFollowUser)
+router.post("/follow/:id", protectRoute, followUnFollowUser)
 
 
 export default router
