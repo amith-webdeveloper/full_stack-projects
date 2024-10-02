6be34f27fdd329c6164b1a4ec5429f159349e76e
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-10-02 12:05:44 +0530

    added some code in backend part

--- Mern_app/backend/controllers/product.controller.js
+++ Mern_app/backend/controllers/product.controller.js
@@ -30,6 +30,11 @@ export const createProduct = async (req, res) => {
 
 export const DeleteProduct = async (req, res) => {
   const { id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ success: false, msg: "inavalid product id" });
+  }
+  
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, msg: "Product deleted" });
--- Mern_app/backend/server.js
+++ Mern_app/backend/server.js
@@ -5,6 +5,7 @@ import ProductRoutes from './routes/product.route.js'
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000
 
 app.use(express.json());
 
@@ -13,9 +14,9 @@ app.use('/api/products' , ProductRoutes)
 
 
 // server listen ===============================================
-app.listen(3000, function () {
+app.listen(port, function () {
   connectDB();
-  console.log("server started at 3000");
+  console.log(`server started at ${port}`);
 });
 
 // amith_mern01
